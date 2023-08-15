f0269e26c7b53ee9a86d4eae35fcf0078d2d97ed
Marsiha <119217147+Marsiha@users.noreply.github.com>
2023-08-15 15:07:42 +0500

    Initial Commit

---
+++ uddor/uddorApp.swift
@@ -0,0 +1,17 @@
+//
+//  uddorApp.swift
+//  uddor
+//
+//  Created by mars uzhanov on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct uddorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
